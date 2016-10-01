03805b24f9bc814181d94421e402805fb18c7aa4
Jonathan Martin <jonathan.martin.android@gmail.com>
2016-10-01 15:53:33 +0200

    Datlib added
    Added datlib headers/lib, but not used yet.

---
+++ DATlib_0.2/sample/DATdemo/externs.h
@@ -0,0 +1,27 @@
+//scrollers
+extern scrollerInfo ffbg_a;
+extern paletteInfo ffbg_a_Palettes;
+extern scrollerInfo ffbg_b;
+extern paletteInfo ffbg_b_Palettes;
+extern scrollerInfo wohd;
+extern paletteInfo wohd_Palettes;
+
+//pictures
+extern pictureInfo ffbg_c;
+extern paletteInfo ffbg_c_Palettes;
+extern pictureInfo terrypict;
+extern paletteInfo terrypict_Palettes;
+extern pictureInfo topleft;
+extern paletteInfo topleft_Palettes;
+extern pictureInfo pointer;
+extern paletteInfo pointer_Palettes;
+extern pictureInfo gradient;
+extern paletteInfo gradient_Palettes;
+extern pictureInfo tf4layers;
+extern paletteInfo tf4layers_Palettes;
+
+//animated sprites
+extern spriteInfo bmary_spr;
+extern paletteInfo bmary_spr_Palettes;
+#include "gfx/bmary_spr/bmary_spr.h"
+
